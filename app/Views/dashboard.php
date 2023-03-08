1106ef0dfe58fff14a16f1063f5dab8f459651b4
Dang Cam <dangcam.pr@outlook.com>
2023-03-08 10:13:19 +0700

    codeigniter4

---
+++ app/Views/dashboard.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to CodeIgniter 4!</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+
+    <!-- STYLES -->
+
+
+</head>
+<body>
+
+<!-- HEADER: MENU + HEROE SECTION -->
+<header>
+
+
+
+</header>
+
+<!-- CONTENT -->
+
+<section>
+
+    <h1>About this page</h1>
+
+    <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+    <p>If you would like to edit this page you will find it located at:</p>
+
+    <pre><code>app/Views/welcome_message.php</code></pre>
+
+    <p>The corresponding controller for this page can be found at:</p>
+
+    <pre><code>app/Controllers/Home.php</code></pre>
+
+</section>
+
+
+
+<footer>
+    <div class="environment">
+
+        <p>Page rendered in {elapsed_time} seconds</p>
+
+        <p>Environment: <?= ENVIRONMENT ?></p>
+
+    </div>
+
+    <div class="copyrights">
+
+        <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
+            open source licence.</p>
+
+    </div>
+
+</footer>
+
+<!-- SCRIPTS -->
+
+
+
+<!-- -->
+
+</body>
+</html>
